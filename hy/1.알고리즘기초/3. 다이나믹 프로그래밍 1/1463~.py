f41b0440d9275ac07b083e8cfc33504771d72fe4
hak0_hand <sonson112828@gmail.com>
2021-09-07 22:41:38 +0900

    0907_hy_이친수

--- hy/1.알고리즘기초/3. 다이나믹 프로그래밍 1/1463~.py
+++ hy/1.알고리즘기초/3. 다이나믹 프로그래밍 1/1463~.py
@@ -260,3 +260,15 @@ else :
                 temp[j] = sum(current[:j+1])
         current = temp.copy()
     print(sum(current)%10007)
+
+
+# 2193 이친수
+n = int(input())
+dp = [0,1] + [0]*(n-1)
+
+if n <= 1 :
+    print(dp[n])
+else :
+    for i in range(2,n+1) :
+        dp[i] = dp[i-1] + dp[i-2]
+    print(dp[n])
