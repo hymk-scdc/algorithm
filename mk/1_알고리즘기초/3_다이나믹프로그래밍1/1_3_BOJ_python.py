5e216a6276c251b594ca72904470e7f5348bfb7d
Mikyeong <leemik1117@gmail.com>
2021-09-15 10:40:56 +0900

    0915_mk_가장큰증가부분수열

--- mk/1_알고리즘기초/3_다이나믹프로그래밍1/1_3_BOJ_python.py
+++ mk/1_알고리즘기초/3_다이나믹프로그래밍1/1_3_BOJ_python.py
@@ -315,3 +315,27 @@ else:
 
 
 # 11053 : 가장 긴 증가하는 부분 수열
+N = int(input(""))
+A = list(map(int, input().split()))
+dp = [1] * N
+for i in range(1, N):
+    for j in range(i):
+        if A[j] < A[i]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(max(dp))
+
+
+# 11055 : 가장 큰 증가 부분 수열
+N = int(input(""))
+A = list(map(int, input().split()))
+dp = A[:]
+for i in range(N):
+    for j in range(i):
+        if A[j] < A[i]:
+            dp[i] = max(dp[i], dp[j]+A[i])
+
+print(max(dp))
+
+
+# 
