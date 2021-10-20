104b9a231270ada3d261a060376683fd63f92095
hak0_hand <sonson112828@gmail.com>
2021-10-20 09:01:31 +0900

    1020_hy_11004까지

--- hy/1.알고리즘기초/5. 정렬/2751~.py
+++ hy/1.알고리즘기초/5. 정렬/2751~.py
@@ -544,3 +544,193 @@ result.sort(key = lambda x : (-int(x[0]),int(x[1]),-int(x[2]),x[3]))
 
 for j in result :
     print(j[3])
+
+
+# 10989 수 정렬하기 3
+'''메모리 초과 '''
+n = int(input())
+
+count = [0] * 10001
+
+for _ in range(n) :
+    i = int(input())
+    count[i] += 1
+result = []
+for i in range(10001) :
+    if count[i] != 0 :
+        for _ in range(count[i]) :
+            result.append(i)
+
+for answer in result :
+    print(answer)
+
+
+'''이걸로 해결'''
+import sys
+n = int(sys.stdin.readline())
+
+count = [0] * 10001
+
+for _ in range(n) :
+    i = int(sys.stdin.readline())
+    count[i] += 1
+
+for i in range(10001) :
+    if count[i] != 0 :
+        for _ in range(count[i]) :
+            print(i)
+
+
+'''병합정렬 메로리 초과'''
+
+def merge_sort(li) :
+    if len(li) < 2 : # 리스트가 1 인 경우
+        return li
+    mid = len(li)//2
+
+    temp1, temp2 = [], []
+    for num in range(mid) :
+        temp1.append(li[num])
+
+    for num in range(mid,len(li)) :
+        temp2.append(li[num])
+
+    # 합치기 전 아이들도 병합정렬이 되어 있는 상태여야 함
+    temp1 = merge_sort(temp1)
+    temp2 = merge_sort(temp2)
+
+    # 병합 정렬한 결과물 담을 곳
+    result = []
+    i,j= 0, 0
+    while (i < len(temp1) and j < len(temp2)) : # 한 쪽이 다 채워지기 전까지
+        if temp1[i] < temp2[j] :
+            result.append(temp1[i])
+            i += 1
+        else :
+            result.append(temp2[j])
+            j += 1
+
+    # 마지막까지 채우기 (while문을 for문으로 바꿨음)
+    if i == len(temp1) : # temp1는 다 채워짐
+        for z in range(j, len(temp2)) :
+            result.append(temp2[z])
+
+    elif j == len(temp2) : # temp2가 다 채워짐
+        for z in range(i, len(temp1)) :
+            result.append(temp1[z])
+
+    return result
+
+n = int(input())
+li = []
+for _ in range(n) :
+    li.append(int(input()))
+
+li = merge_sort(li)
+
+for i in li : print(i)
+
+'''힙정렬 메모리초과'''
+N = int(input())
+li = []
+for _ in range(N) :
+    li.append(int(input()))
+
+
+def heapify(li) : # 최대힙구조 만들기
+    for i in range(1, len(li)) :
+        child = i
+        while child != 0 :
+            root = (child-1) // 2 # i번째 노드의 부모노드
+            if li[root] < li[child] : # 부모노드보다 자식 노드가 큰 경우
+                li[root], li[child] = li[child], li[root]
+            child = root
+    return li
+
+def heap_sort(li) :
+    end = len(li) - 1
+    for i in range(end,-1, -1) :
+        li[0], li[i] = li[i], li[0] # 가장 큰 값을 가장 마지막으로 보낸다
+        root = 0
+        child = 1
+        while child < i :
+            child = 2*root + 1
+            if child < i-1 and li[child] < li[child+1] :
+                child +=1
+            if child < i and li[root] < li[child] : # 부모노드보다 자식 노드가 큰 경우
+                li[root], li[child] = li[child], li[root]
+            root = child
+
+heapify(li)
+heap_sort(li)
+
+for i in li : print(i)
+
+'''삽입정렬'''
+n = int(input())
+li = []
+for _ in range(n) : 
+    li.append(int(input())) 
+
+def insert_sort(li) :
+    for i in range(1,len(li)) :
+        for j in range(i,0,-1) :
+            if li[j-1] > li[j] :
+                li[j-1], li[j] = li[j], li[j-1]
+    return li
+
+li = insert_sort(li)
+for i in li : print(i)
+
+
+'''sort()'''
+n = int(input())
+li = []
+for _ in range(n) :
+    li.append(int(input()))
+li.sort()
+for i in li : print(i)
+
+
+'''버블 정렬 메모리 초과'''
+n = int(input())
+li = []
+for _ in range(n) :
+    li.append(int(input()))
+    for i in range(len(li)-1,0,-1) :
+        if li[i] < li[i-1] :
+            li[i], li[i-1] = li[i-1], li[i]
+        else :
+            break
+
+for i in li : print(i)
+
+
+# 11652 카드
+import sys
+n = int(sys.stdin.readline())
+li = []
+result = []
+for _ in range(n) :
+    num = int(sys.stdin.readline())
+    if num in li :
+        for count in result :
+            if count[1] == num :
+                count[0] +=1
+    else :
+        li.append(num)
+        result.append([1,num])
+
+result.sort(key = lambda x : (-x[0], x[1]))
+
+print(result[0][1])
+
+
+# 11004 K번째 수
+import sys
+n, k = map(int, sys.stdin.readline().split())
+li = list(map(int, sys.stdin.readline().split()))
+
+li.sort()
+
+print(li[k-1])
