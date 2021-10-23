3b8f196233cdff03cb832cc42b44eeb2257bfaa5
Mikyeong <leemik1117@gmail.com>
2021-10-23 18:07:44 +0900

    1023_mk

--- mk/1_알고리즘기초/5_정렬/1_5_BOJ_python.py
+++ mk/1_알고리즘기초/5_정렬/1_5_BOJ_python.py
@@ -273,3 +273,25 @@ for i in range(1,N+2):
             TF = True
             list_1[j], list[j+1] = list[j+1], list_1[j]
     if TF == False:
+=======
+'''
+pypy 말고 Python3으로 했더니 됨
+'''
+import sys
+
+N = int(sys.stdin.readline())
+a = [0] * 10001
+
+for _ in range(N):
+    a[int(sys.stdin.readline())] += 1
+
+for i in range(10001):
+    if a[i] != 0:
+        for _ in range(a[i]):
+            print(i)
+
+
+# 11652 : 카드
+
+
+>>>>>>> Stashed changes
